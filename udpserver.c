68787c50b773e4c64ff5850960df6b345d136531
rkminz <rajeevkrminz@gmail.com>
2020-05-07 14:06:57 +0530

    Initial version of udpserver

---
+++ udpserver.c
@@ -0,0 +1,70 @@
+/* udpserver.c */ 
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main()
+{
+        int sock;
+        int addr_len, bytes_read;
+        char recv_data[1024];
+        struct sockaddr_in server_addr , client_addr;
+        char *server_ip = "127.0.0.1";
+
+
+        if ((sock = socket(AF_INET, SOCK_DGRAM, 0)) == -1) {
+            perror("Socket");
+            exit(1);
+        }
+
+        server_addr.sin_family = AF_INET;
+        server_addr.sin_port = htons(5000);
+        server_addr.sin_addr.s_addr = inet_addr(server_ip);
+        bzero(&(server_addr.sin_zero),8);
+
+
+       // if (bind(sock,(struct sockaddr *)&server_addr,
+       //     sizeof(struct sockaddr)) == -1)
+       // {
+       //     perror("Bind");
+       //     exit(1);
+       // }
+
+        addr_len = sizeof(struct sockaddr);
+		
+	    printf("\nUDPServer Waiting for client on port 5000");
+        fflush(stdout);
+  char ch[10];
+
+	while (1)
+	{
+      //bytes_read = recvfrom(sock,recv_data,1024,0,
+	  //              (struct sockaddr *)&client_addr, &addr_len);
+	  //
+
+	  //recv_data[bytes_read] = '\0';
+
+      //printf("\n(%s , %d) said : ",inet_ntoa(client_addr.sin_addr),
+      //                                 ntohs(client_addr.sin_port));
+      //printf("%s", recv_data);
+
+        server_addr.sin_family = AF_INET;
+        server_addr.sin_port = htons(5001);
+        server_addr.sin_addr.s_addr = inet_addr(server_ip);
+        bzero(&(server_addr.sin_zero),8);
+        scanf(" %s",recv_data);
+
+       sendto(sock, recv_data, strlen(recv_data), 0,
+              (struct sockaddr *)&server_addr, sizeof(struct sockaddr));
+
+	  fflush(stdout);
+    }
+        return 0;
+}
